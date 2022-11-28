431ab27da91cb510ba7b2aba31fc9163561ebb04
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2022-11-28 08:28:27 +0000

    made Table a normal component (no makeTable)

--- index.jsx
+++ index.jsx
@@ -10,10 +10,10 @@ import PropTypes from "./prop-types";
 import InnerEnum from "./Enum";
 import InnerPercent from "./Percent";
 import InnerTooltipCircle from "./TooltipCircle";
-import innerMakeTable from "./makeTable";
+import InnerTable from "./Table";
 export { _get } from "./utils";
 
-export const makeTable = innerMakeTable;
+export const Table = InnerTable;
 export const MyPropTypes = PropTypes;
 export const Enum = InnerEnum;
 export const Percent = InnerPercent;
