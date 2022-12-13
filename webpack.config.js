e41146beb5ef265656617b8413b88f425e094899
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2022-12-13 18:13:45 +0000

    Don't remove licence text

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,5 @@ module.exports = function (env) {
     library: true,
   });
 
-  config.plugins.push(new (class {
-    apply(compiler) {
-      compiler.hooks.done.tap('Cleanup', () => {
-        fs.unlinkSync('build/main.js.LICENSE.txt');
-      });
-    }
-  })());
-
   return config;
 };
