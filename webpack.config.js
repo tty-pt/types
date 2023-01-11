7c370836243c0c8bd038c6c10c66314fd253f4cc
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2023-01-11 15:45:27 +0000

    Update @tty-pt/styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-const makeConfig = require("react-noscripts/webpack.config");
+const makeConfig = require("@tty-pt/scripts/webpack.config");
 
 module.exports = function (env) {
   let config = makeConfig({
     ...env,
+    development: true,
     library: true,
   });
 
