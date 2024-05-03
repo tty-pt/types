7ebfad8f65155a7d59eec63a0d408fc261236aa0
Paulo Andre Azevedo Quirino <paulo.quirino@mov.ai>
2024-05-03 08:07:07 +0100

    Percent index must be in range

--- src/Percent.jsx
+++ src/Percent.jsx
@@ -3,11 +3,14 @@ import PropTypes from "prop-types";
 import componentsSub from "./componentsSub";
 import MyPropTypes from "./prop-types";
 
+const min = (a, b) => (a < b ? a : b);
+const max = (a, b) => (a > b ? a : b);
+
 export function Percent(props) {
   const { icons, level } = props;
   const { Tooltip } = componentsSub.use();
   const index = Math.floor(level * (icons.length - 1) / 100);
-  const Icon = icons[index];
+  const Icon = icons[max(0, min(index, icons.length - 1))];
   return <Tooltip title={level.toFixed(2) + "%"}><Icon /></Tooltip>;
 }
 
